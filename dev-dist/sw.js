6345ce460262094d98ff6630807975633edd8e7c
Sebastian <Sebmagsen@gmail.com>
2026-02-23 10:41:20 +0100

    Update: <kort beskrivelse av hva du endret>

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.3qst7d30mpo"
+    "revision": "0.2msig6plkts"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
