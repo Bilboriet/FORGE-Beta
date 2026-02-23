fe3667e65c37b5154db6f45c8ba86f6f382cf93c
Sebastian <Sebmagsen@gmail.com>
2026-02-23 10:12:07 +0100

    Golden baseline: dev UI + auth working

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.5sagaba8itg"
+    "revision": "0.3qst7d30mpo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
